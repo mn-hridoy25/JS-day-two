577cd1a232d2bd61c89b7e9c6fb525f92ba2604c
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-08 22:43:53 +0600

    first commit

---
+++ strign.js
@@ -0,0 +1,20 @@
+// Some string declare system:
+
+
+var str = 'string'
+var str1 = "string"
+var str2 = `string` // Only for ES6
+
+var str4 = String(5654)
+var str5 = String("This is a string")
+var str6 = String(6.454)
+
+
+
+
+console.log(str5);
+console.log(str6);
+console.log(str4);
+console.log(str1);
+console.log(str2);
+console.log(str);

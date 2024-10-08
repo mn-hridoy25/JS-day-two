ceafc05adb270002b6d993addb8825f80300da01
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 02:44:19 +0600

    arithmetic operators added

---
+++ typeConversion.js
@@ -0,0 +1,15 @@
+//kuno data ke ek data type theke onno data type e convert korar process kei type conversion bole.
+// Example 1 : string to number-
+
+var str = '1234';
+var num = 10;
+console.log(Number.parseInt(str) + num);
+
+// Example 2: number to string-
+
+var num1 = 125;
+var str1 = 6545;
+console.log(num1.toString() + str1.toString());
+
+
+

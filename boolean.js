5d1764c6d92660f0ffdd1b8641a3cb5ddce63c2c
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-08 23:01:59 +0600

    boolean example added

---
+++ boolean.js
@@ -0,0 +1,25 @@
+// Boolean:
+// 1. true
+// 2. false
+
+// example:
+
+const isRaining = true;
+
+if (isRaining) {
+    console.log("I don't go to school");
+}
+else {
+    console.log("I go to school");
+}
+
+
+//  Example 2:
+
+const dateExpired = false
+
+if (dateExpired) {
+    console.log("No one can eat this food.");
+} else {
+    console.log("Any one can eat this food.");
+}

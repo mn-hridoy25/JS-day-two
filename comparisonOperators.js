9b174771b261caf0b324e9569218fc6d064bb8b1
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 17:38:53 +0600

    comparison operators added

--- comparisonOperators.js
+++ comparisonOperators.js
@@ -3,6 +3,8 @@
 // 2. >
 // 3. <
 // 4. !=
+// 5. >=
+// 6. <=
 
 var a = 20;
 var b = 30;
@@ -12,3 +14,6 @@ console.log(a == b); // a ar b soman hole true return korbe
 console.log(a > b); // a theke b suto hole true return korbe
 console.log(a < b); // a theke b boro hole true return korbe
 console.log(a != b); // a ar b soman na hole true return korbe
+console.log(a >= b); // a jodi b er theke boro othoba soman hoy tobe true return korbe
+console.log(a <= b); // a jodi b er theke suto othoba soman hoy tobe true return korbe
+

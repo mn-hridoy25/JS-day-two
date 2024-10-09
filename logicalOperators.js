eacea1f459b3f7c6b309bf90da5c2336fb0dc19d
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 17:49:59 +0600

    logical operators added

---
+++ logicalOperators.js
@@ -0,0 +1,20 @@
+// Logical operators list:-
+// 1. && - sobgulu condition true hote hobe
+// 2. || - jekuno ekti condition true hote hobe
+// 3. ! - negative value return kore
+
+var a = 25;
+var b = 30;
+var c = 15;
+
+
+// example:-
+console.log(a > c && b > a);
+console.log(a > b || b > c);
+console.log(a != b);
+
+
+
+
+
+

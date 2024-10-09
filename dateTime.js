b5c15735e20a1ec86638e53c3326eab6959bd3f9
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 18:27:06 +0600

    date time function added

--- dateTime.js
+++ dateTime.js
@@ -6,6 +6,15 @@ console.log(date.toDateString()); // present date string e output dibe
 console.log(date.toTimeString()); //present time string e output dibe
 console.log(date.toLocaleTimeString()); //local time string e output dibe
 console.log(date.toLocaleDateString()); //local date string e output dibe
+console.log(date.getDate());
+console.log(date.getDay()); //sunday theke start hoy ebong count hoy 0 theke
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMonth()); //0 theke count hoy tai 1month kom dekhabe
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+
 
 
 

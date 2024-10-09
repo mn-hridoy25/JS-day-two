f0a82593c248eb2d48f06fefb5d1b41c2e80c2ef
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 18:20:50 +0600

    date time function added

---
+++ dateTime.js
@@ -0,0 +1,11 @@
+var date = new Date();
+
+console.log(date); //present date and time output dibe
+
+console.log(date.toDateString()); // present date string e output dibe
+console.log(date.toTimeString()); //present time string e output dibe
+console.log(date.toLocaleTimeString()); //local time string e output dibe
+console.log(date.toLocaleDateString()); //local date string e output dibe
+
+
+

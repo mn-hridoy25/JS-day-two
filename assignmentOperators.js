28a6d77cf7054f978a4e0acf5a4012087217110c
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 17:19:07 +0600

    assignment operators added

---
+++ assignmentOperators.js
@@ -0,0 +1,24 @@
+// assignment operator-
+// kuno variable er value ke assign korar jonno asssignment operator use kora hoy. jemon:
+var a = 33;
+var b = 11;  // a ar b er value ke assign korar jonno ekhane = operator use kora hoyeche.
+
+
+a -= b; // a -= b; means- a = a - b;
+console.log(a);
+
+a += b; //a += b; means- a = b + a;
+console.log(b);
+
+
+a /= b; //a /= b; means- a = a/b;
+console.log(a);
+
+
+a *= b; //a *= b; means- a=a*b;
+console.log(a);
+
+
+a %= b; // a %= b; means- a = a % b;
+console.log(a);
+

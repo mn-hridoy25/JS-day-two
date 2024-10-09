4477d66a68f61a0c2103a1fc729e6f1cf291fe09
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 18:13:49 +0600

    math function added

---
+++ math.js
@@ -0,0 +1,27 @@
+// Some uses of math function:
+
+var a = 20;
+var b = 30;
+var c = 40;
+var d = 50;
+var e = -60;
+var f = 5.6614;
+var g = 25;
+
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.abs(e));
+console.log(Math.floor(f)); // ager integer value dibe
+console.log(Math.min(a, b, c, d));
+console.log(Math.max(a, b, c, d));
+console.log(Math.ceil(f)); //porer integer value dibe
+console.log(Math.round(f)); //kasakasi je integer value thake setai dibe
+console.log(Math.pow(a, 2)); // power
+console.log(Math.sqrt(g)); // square root korbe
+console.log(Math.random()); // jekuno random number output dibe
+console.log(Math.random() * 50 + 1); //1 theke 50 er moddhe output dibe
+console.log(Math.round(Math.random() * 50 + 1)); //1 theke 50 er moddhe integer number output dibe
+
+
+

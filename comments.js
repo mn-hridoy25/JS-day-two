0994ba7f9b4c718db7b721b6ecfb067b494930c0
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-09 18:33:02 +0600

    comments added

---
+++ comments.js
@@ -0,0 +1,12 @@
+// single line comment:-
+
+            // this is a single line comment
+
+
+//munltiline comments:-
+
+/*
+This is the first line.
+This is the second line.
+This is the third line.
+*/
